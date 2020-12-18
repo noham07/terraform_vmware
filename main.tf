aac4b60acc46a087b12493b1158dd209758180dd
Noham <noham.medyouni@gmail.com>
2020-12-18 14:45:16 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,95 @@
+#==================== #
 # Deploying VMware VM #
+# =================== #
+
+#The variables are all defined in the variables.tf file. The values assigned to the variables are set in the tfvars file
 
 provider "vsphere" {
-  user                  = "XXXXXXXXXXXXXX"
-  password              = "YYYYYYYYYYYYYY"
-  vsphere_server        = "OVH-vCenter"
-   # If you have a self-signed cert
-  allow_unverified_ssl = var.vsphere-unverified-ssl
+  #https://www.terraform.io/docs/providers/vsphere/index.html
+  user                 = var.vsphere_user
+  password             = var.vsphere_password
+  vsphere_server       = var.vsphere_server
+  # If you have a self-signed cert
+  allow_unverified_ssl = true
 }
- 
- 
-/*================
-Deploy Virtual Machimes
-=================*/
- 
-module "VMs" {
-  source = "../VMs"
-  data_center         = var.data_center
-  cluster             = var.cluster
-  workload_datastore  = var.workload_datastore
-  compute_pool        = var.compute_pool 
+
+data "vsphere_datacenter" "dc" {
+  #https://www.terraform.io/docs/providers/vsphere/d/datacenter.html
+  name = var.vsphere_datacenter
+}
+
+data "vsphere_datastore" "datastore" {
+  #https://www.terraform.io/docs/providers/vsphere/d/datastore.html
+  name          = var.datastore
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_compute_cluster" "cluster" {
+  #https://www.terraform.io/docs/providers/vsphere/d/compute_cluster.html
+  name          = var.cluster
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_network" "network" {
+  #https://www.terraform.io/docs/providers/vsphere/d/network.html
+  name          = var.portgroup
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_virtual_machine" "template" {
+  #https://www.terraform.io/docs/providers/vsphere/d/virtual_machine.html
+  name          = var.template_name
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+resource "vsphere_virtual_machine" "vm" {
+  count = var.vm_count
+  name             = "${var.vm_name}-${count.index + 1}"
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore_id     = data.vsphere_datastore.datastore.id
+  folder           = var.deploy_vsphere_folder
+
+  num_cpus = var.vcpu_count
+  memory   = var.memory
+  guest_id = data.vsphere_virtual_machine.template.guest_id
+
+  scsi_type = data.vsphere_virtual_machine.template.scsi_type
+
+  network_interface {
+    network_id   = data.vsphere_network.network.id
+    adapter_type = data.vsphere_virtual_machine.template.network_interface_types[0]
+  }
+
+  disk {
+    label            = "disk0"
+    size             = data.vsphere_virtual_machine.template.disks.0.size
+    eagerly_scrub    = data.vsphere_virtual_machine.template.disks.0.eagerly_scrub
+    thin_provisioned = data.vsphere_virtual_machine.template.disks.0.thin_provisioned
+  }
+
+  clone {
+    template_uuid = data.vsphere_virtual_machine.template.id
+
+    customize {
+      #https://www.terraform.io/docs/providers/vsphere/r/virtual_machine.html#linux-customization-options
+      linux_options {
+        host_name = var.vm_name
+        domain    = var.domain_name
+      }
+
+      #This example uses DHCP. To switch to static IP addresses, comment the line below...
+      #network_interface {}
+
+      #... and uncomment this bloc
+      
+      network_interface {
+        ipv4_address = var.vm_ip
+        ipv4_netmask = var.vm_cidr
+    }
+      ipv4_gateway = var.default_gw
+      dns_server_list = ["192.168.1.16"]
+
+    }
+
+  }
 }
